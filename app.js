a2b1f83e74d9345f0b9c7ec999e2a9d8a9e93fe1
Cardo Tisler <cardotisler@live.com>
2021-12-04 16:15:00 +0200

    add start script for package.json

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var express = require('express'),
     app = express(),
     postsRoutes = require("./routes/posts");
 
+const { Pool } = require("pg");
+const pg = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+})
+
+Pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    Pool.end();
+})
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
+app.use(express.json());
 app.use(postsRoutes);
 
 app.listen(3000, process.env.IP, function(){
